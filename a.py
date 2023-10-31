0ca5b8befdadf830c99de3f2ccee3ecf4612e3b2
A <20ise0104@vvce.ac.in>
2023-10-31 16:08:56 +0530

    version1

---
+++ a.py
@@ -0,0 +1,8 @@
+def factorial_recursive(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial_recursive(n - 1)
+num = 5
+result = factorial_recursive(num)
+print(f"The factorial of {num} is {result}")
